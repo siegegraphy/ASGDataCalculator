a4a66e61afdf4c7cf497dbf223021aae9b4af32d
siegegraphy <siegegraphy@gmail.com>
2026-03-02 16:39:19 +0100

    Delete backend directory

--- backend/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import SavedCalculator
-
-@admin.register(SavedCalculator)
-class SavedCalculatorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'user', 'created_at')
-    list_filter = ('user',)
-    search_fields = ('name',)
